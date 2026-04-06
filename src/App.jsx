0cb901ff6eeca9079c9d0cc373eb7a676a5a752d
annajulia17382 <40851@raphaeldisanto.com.br>
2026-04-06 08:57:24 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import './App.css'
+
+
+export default function App() {
+ 
+  return (
+    <>
+       <main>
+        <section>
+        <div className="comp">
+          <icone>className= "icone"</icone>
+          <h1>instagram</h1>
+          <p>fotos e videos</p>
+        </div>
+
+
+        <div className="comp">
+          <icone>className= "icone"</icone>
+          <h1>whatsapp</h1>
+          <p>conversas individuais e em gupo</p>
+        </div>
+       
+        <div className="comp">
+          <icone>className= "icone" </icone>
+          <h1>discord</h1>
+          <p>discussoes por topicos</p>
+        </div>
+        </section>
+      </main>
+
+    </>
+  )
+}
